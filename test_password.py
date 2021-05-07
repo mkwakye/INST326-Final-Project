fbec99dd7018304e4ff7e95e706ec958876594bf
tokyodrift7 <mkwakye99@gmail.com>
2021-05-07 14:29:02 -0500

    Create test_password.py

---
+++ test_password.py
@@ -0,0 +1,30 @@
+""" Pytest for get_cost() """
+
+"""
+Names: Nicholas Rudiger, Michael Kwakye
+Emails: nicrudiger@gmail.com, mkwakye99@gmail.com
+Assignment: Exercise 2
+Date: 2_16_21
+
+"""
+
+import sys
+import password as pw
+
+def test_store():
+    """ 
+    function that tests some
+    cases in the Store class within the password program
+    """
+    
+def test_checker():
+    """ 
+    function that tests some
+    cases in the Checker class within the password program
+    """
+
+def test_generate():
+    """ 
+    function that tests some
+    cases in the Generator class within the password program
+    """
