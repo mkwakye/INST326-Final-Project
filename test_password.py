289a3f7f25d04ca1dde4e19c6ccc6963fd89f3e3
tokyodrift7 <mkwakye99@gmail.com>
2021-05-08 16:40:24 -0500

    Update test_password.py

--- test_password.py
+++ test_password.py
@@ -1,13 +1,5 @@
 """ Pytest for password.py """
 
-"""
-Names: Nicholas Rudiger, Michael Kwakye
-Emails: nicrudiger@gmail.com, mkwakye99@gmail.com
-Assignment: Exercise 2
-Date: 2_16_21
-
-"""
-
 import sys
 import password as pw
 
